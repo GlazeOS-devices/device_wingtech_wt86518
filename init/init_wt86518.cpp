c0b1bd15f491d0d6f5f4487dae428b8faa88f193
karthick111 <karthickrko61@gmail.com>
2017-01-11 07:46:10 +0100

    Wt86518: Mokee Bringup

--- init/init_wt86518.cpp
+++ init/init_wt86518.cpp
@@ -1,7 +1,9 @@
 /*
-   Copyright (c) 2015, The Linux Foundation. All rights reserved.
-   Copyright (C) 2015, The CyanogenMod Project
+   Copyright (c) 2016, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016, The CyanogenMod Project
    Copyright (C) 2015, Ketut P. Kumajaya
+   Copyright (C) 2016, Nikolai Petrenko
+   Copyright (C) 2017, The LineageOS Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -29,211 +31,107 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-#include "property_service.h"
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
 #include "vendor_init.h"
+#include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#include "init_msm8916.h"
 
-#define ALPHABET_LEN 256
-#define KB 1024
+static std::string board_id;
 
-#define IMG_PART_PATH "/dev/block/bootdevice/by-name/modem"
-#define IMG_VER_STR "QC_IMAGE_VERSION_STRING="
-#define IMG_VER_STR_LEN 24
-#define IMG_VER_BUF_LEN 255
-#define IMG_SZ 32000 * KB    /* MMAP 32000K of modem, modem partition is 64000K */
-
-static char board_id[32];
-
-static void import_kernel_nv(const std::string& key, const std::string& value, bool for_emulator) {
-
-    char in_str[32], *board_value, *ptr;
-    int count = 0;
-
-    // board_id=S88047C1:board_vol=1047620
-    // The above string can be broken down into three pieces
-    // key should contain "board_id" and value contains "S88047C1:board_vol"
-
-    if (strcmp(key.c_str(), "board_id")) {
-	/* not board_id */
-    	return;
-    }
-
-    strncpy(in_str, value.c_str(), sizeof(in_str));
-    if (in_str[0] != '\0') {
-	ptr = in_str;
-	// delimiter or eol found
-	do {
-	    if (*ptr == ':') break;
-	    if (*ptr == '\0') break;
-	    ++count;
-	    ++ptr;
-	} while (ptr);
-
-	strncpy(board_id, value.c_str(), count);
-	board_id[count]='\0';
-	ERROR("\n **** READ BOARDID=%s **** \n",board_id);
-    }
-
-    return;
-}
-
-/* Boyer-Moore string search implementation from Wikipedia */
-
-/* Return longest suffix length of suffix ending at str[p] */
-static int max_suffix_len(const char *str, size_t str_len, size_t p) {
-    uint32_t i;
-
-    for (i = 0; (str[p - i] == str[str_len - 1 - i]) && (i < p); ) {
-        i++;
-    }
-
-    return i;
-}
-
-/* Generate table of distance between last character of pat and rightmost
- * occurrence of character c in pat
- */
-static void bm_make_delta1(int *delta1, const char *pat, size_t pat_len) {
-    uint32_t i;
-    for (i = 0; i < ALPHABET_LEN; i++) {
-        delta1[i] = pat_len;
-    }
-    for (i = 0; i < pat_len - 1; i++) {
-        uint8_t idx = (uint8_t) pat[i];
-        delta1[idx] = pat_len - 1 - i;
-    }
-}
-
-/* Generate table of next possible full match from mismatch at pat[p] */
-static void bm_make_delta2(int *delta2, const char *pat, size_t pat_len) {
-    int p;
-    uint32_t last_prefix = pat_len - 1;
-
-    for (p = pat_len - 1; p >= 0; p--) {
-        /* Compare whether pat[p-pat_len] is suffix of pat */
-        if (strncmp(pat + p, pat, pat_len - p) == 0) {
-            last_prefix = p + 1;
-        }
-        delta2[p] = last_prefix + (pat_len - 1 - p);
-    }
-
-    for (p = 0; p < (int) pat_len - 1; p++) {
-        /* Get longest suffix of pattern ending on character pat[p] */
-        int suf_len = max_suffix_len(pat, pat_len, p);
-        if (pat[p - suf_len] != pat[pat_len - 1 - suf_len]) {
-            delta2[pat_len - 1 - suf_len] = pat_len - 1 - p + suf_len;
-        }
-    }
-}
-
-static char * bm_search(const char *str, size_t str_len, const char *pat,
-        size_t pat_len) {
-    int delta1[ALPHABET_LEN];
-    int delta2[pat_len];
-    int i;
-
-    bm_make_delta1(delta1, pat, pat_len);
-    bm_make_delta2(delta2, pat, pat_len);
-
-    if (pat_len == 0) {
-        return (char *) str;
-    }
-
-    i = pat_len - 1;
-    while (i < (int) str_len) {
-        int j = pat_len - 1;
-        while (j >= 0 && (str[i] == pat[j])) {
-            i--;
-            j--;
-        }
-        if (j < 0) {
-            return (char *) (str + i + 1);
-        }
-        i += MAX(delta1[(uint8_t) str[i]], delta2[j]);
-    }
-
-    return NULL;
-}
-
-static int get_img_version(char *ver_str, size_t len) {
-    int ret = 0;
-    int fd;
-    char *img_data = NULL;
-    char *offset = NULL;
-
-    fd = open(IMG_PART_PATH, O_RDONLY);
-    if (fd < 0) {
-        ret = errno;
-        goto err_ret;
-    }
-
-    img_data = (char *) mmap(NULL, IMG_SZ, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (img_data == (char *)-1) {
-        ret = errno;
-        goto err_fd_close;
-    }
-
-    /* Do Boyer-Moore search across IMG data */
-    offset = bm_search(img_data, IMG_SZ, IMG_VER_STR, IMG_VER_STR_LEN);
-    if (offset != NULL) {
-        strncpy(ver_str, offset + IMG_VER_STR_LEN, len);
-    } else {
-        ret = -ENOENT;
-    }
-
-    munmap(img_data, IMG_SZ);
-err_fd_close:
-    close(fd);
-err_ret:
-    return ret;
-}
-
-void vendor_load_properties()
+static void import_entire_kernel_cmdline(bool in_qemu,
+                           const std::function<void(const std::string&, bool)>& fn)
 {
-    char modem_version[IMG_VER_BUF_LEN];
-    int rc;
+    std::string cmdline;
+    android::base::ReadFileToString("/proc/cmdline", &cmdline);
 
-    std::string product = property_get("ro.product.name");
-    if ((strstr(product.c_str(), "wt86518") == NULL))
+    for (const auto& entry : android::base::Split(android::base::Trim(cmdline), " ")) {
+        fn(entry, in_qemu);
+    }
+}
+
+static void import_cmdline(const std::string& name, bool for_emulator)
+{
+    if (name.empty())
         return;
 
-    import_kernel_cmdline(0, import_kernel_nv);
-    property_set("ro.product.board", board_id);
-    ERROR("Detected board ID=%s\n", board_id);
+    std::string::size_type pos = name.find('=');
+    std::string value = name.substr(pos + 1);
+    if (strstr(name.c_str(), "board_id") != NULL) {
+        pos = value.find(':');
+        board_id = value.substr(0, pos);
+    }
+}
 
+void init_target_properties()
+{
+    std::string product = property_get("ro.product.name");
+    if (strstr(product.c_str(), "wt86518") == NULL)
+        return;
+
+    import_entire_kernel_cmdline(0, import_cmdline);
+
+    if (board_id == "S88047E1") {
+
+        property_set("ro.telephony.default_network", "9,1");
+    } else if (board_id == "S88047D1") {
         property_set("ro.build.product", "Lenovo");
-        property_set("ro.product.device", "Lenovo");
+        property_set("ro.product.device", "A6000");
         property_set("ro.product.model", "Lenovo A6000");
         property_set("ro.product.name", "Lenovo A6000");
         property_set("ro.telephony.default_network", "9,1");
-        property_set("telephony.lteOnCdmaDevice", "0");
+    } else if (board_id == "S88047C1") {
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "9,1");
+        property_set("persist.dbg.volte_avail_ovr", "1");
+        property_set("persist.dbg.vt_avail_ovr", "1");
+    } else if (board_id == "S88047B2") {
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "22,1");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("persist.radio.sglte.eons_domain", "ps");
+    } else if (board_id == "S88047B1") {
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "22,1");
+        property_set("telephony.lteOnCdmaDevice", "1");
+        property_set("persist.radio.sglte.eons_domain", "ps");
+    } else if ((board_id =="S86047A1") || (board_id == "S86047A1_CD")) {
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "9,1");
+    } else if ((board_id == "S86047A2") || (board_id == "S86047A2_CD")) {
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "9,1");
+    } else { /* including S88047A2 and S88047A1 */
+        property_set("ro.build.product", "Lenovo");
+        property_set("ro.product.device", "A6000");
+        property_set("ro.product.model", "Lenovo A6000");
+        property_set("ro.product.name", "Lenovo A6000");
+        property_set("ro.telephony.default_network", "9,1");
     }
 
     /* Unified description and fingerprint for now */
-    property_set("ro.build.description", "wt86518-userdebug 5.0.2 LRX22G release-keys");
-    property_set("ro.build.fingerprint", "Lenovo/wt86518/wt86518:5.0.2/LRX22G:userdebug/release-keys");
-
-    ERROR("Setup %s properties done!\n", board_id);
-
-    rc = get_img_version(modem_version, IMG_VER_BUF_LEN);
-    if (!rc) {
-        property_set("gsm.version.baseband", modem_version);
-        ERROR("Detected modem version=%s\n", modem_version);
-    }
-
-    return;
+    property_set("ro.build.description", "wt86518-user 5.0.2 LRX22G release-keys");
+    property_set("ro.build.fingerprint", "Lenovo/wt86518/wt86518:5.0.2/LRX22G/vibe-ui-2.0/release-keys");
 }
