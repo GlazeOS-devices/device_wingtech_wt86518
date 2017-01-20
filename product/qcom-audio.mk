01592825cb0f2268be5fbcf788ef284f29fe4c86
Harshit Jain <harshitjain6751@gmail.com>
2017-01-20 10:27:14 +0530

    wt86518: Disbale Fluence

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qc.sdk.audio.fluencetype=fluence \
-    persist.audio.fluence.voicecall=true \
+    ro.qc.sdk.audio.fluencetype=none \
+    persist.audio.fluence.voicecall=false \
     persist.audio.fluence.voicerec=false \
     persist.audio.fluence.speaker=false
